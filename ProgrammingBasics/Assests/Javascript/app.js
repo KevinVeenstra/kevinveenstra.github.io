92c05b5aa7495b087c0e7709c8a6f883fe960511
KevinVeenstra <42801649+KevinVeenstra@users.noreply.github.com>
2018-09-17 14:28:31 +0200

    Programming Basics

---
+++ ProgrammingBasics/Assests/Javascript/app.js
@@ -0,0 +1,39 @@
+/* const firstName = "Kevin", lastName = "veenstra", age = "33";
+
+console.log("Mijn naam is " + firstName + " " + lastName + " en ik ben " + age + " jaar oud");
+
+let priceOfAnApple = 0.59;
+let numberOfApples = 200;
+
+let totalPriceOfApples = priceOfAnApple * numberOfApples;
+
+// console.log("Total price of Apples " + totalPriceOfApples);
+console.log(`Total price of Apples : ${totalPriceOfApples}`);
+
+
+let hzString = `Please visit the HZ university of applied science`;
+console.log(hzString);
+hzString.replace(`applied`, `mad`);
+let hzReplacedString = hzString.replace(`applied`,`mad`);
+console.log(hzReplacedString); */
+
+// Opdracht 1
+let number = 12;
+console.log("The given number is : " + number)
+
+number = number % 2 == 0;
+console.log("The number is even : " + number);
+
+// Opdracht 2
+let stringOpdracht2 = "Programming is not so cool";
+console.log(stringOpdracht2);
+
+stringOpdracht2.replace(`not `, ``);
+let Opdracht2ReplacedString = stringOpdracht2.replace(`not `, ``)
+console.log(Opdracht2ReplacedString);
+
+// Opdracht 3
+let givenValeu = "1400";
+givenValeu = "1400" === "Ik woon in naboo";
+console.log(givenValeu);
+console.log(typeof givenValeu);
