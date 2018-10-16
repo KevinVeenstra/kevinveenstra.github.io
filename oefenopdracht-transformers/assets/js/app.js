f9b43be36439a39c09d857a6b4394dc77547a3b9
KevinVeenstra <42801649+KevinVeenstra@users.noreply.github.com>
2018-10-16 12:09:48 +0200

    raad het plaatje + nieuwe transformer opdracht

---
+++ oefenopdracht-transformers/assets/js/app.js
@@ -0,0 +1,36 @@
+// All global variables
+const imageFolder = "./assets/images/";
+const imagez = [{
+        position: 1,
+        name: 'transformers',
+        source: 'transformers-1.png'
+    },
+    {
+        position: 2,
+        name: 'transformers',
+        source: 'transformers-2.png'
+    },
+    {
+        position: 3,
+        name: 'transformers',
+        source: 'transformers-3.png'
+    },
+    {
+        position: 4,
+        name: 'transformers',
+        source: 'transformers-4.png'
+    }
+];
+
+// laad de plaatjes op de pagina
+
+
+// laad de plaatjes in random volgorde op de pagina
+
+// maak de plaatjes clickbaar
+
+// vergelijk de volgorde
+
+// poging timer
+
+// eind resultaat
